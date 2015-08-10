bf9ca51b79b664c06e78941a8bbe628d83d995ca
bsanchezdev <bsanchezdev@gmail.com>
2015-08-10 16:12:30 -0400

    test 2

--- test_upload_change_netbeans.php
+++ test_upload_change_netbeans.php
@@ -1,5 +1,6 @@
 <?php
 echo "Test de funcionalidad Github con netbeans";
+echo "test 2";
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
