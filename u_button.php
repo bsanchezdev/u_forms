ed1fcc073de099c1197c590ab0a0cfdc8c1cd380
bsanchezdev <bsanchezdev@gmail.com>
2015-08-13 20:50:15 -0400

    Test Commit

--- u_button.php
+++ u_button.php
@@ -1,15 +1,8 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Description of u_div
  *
- * @author nocturnus
+ * @author Benjamin Sanchez Cardenas nocturnus
  */
 class u_button extends u_ {
     
